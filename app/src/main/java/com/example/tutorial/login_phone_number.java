c59df4f0e0821e71f4e31eb7bf2bdb6f891dc38e
TommasoBiadene <61515795+TommasoBiadene@users.noreply.github.com>
2023-11-29 07:12:05 +0100

    p

--- app/src/main/java/com/example/tutorial/login_phone_number.java
+++ app/src/main/java/com/example/tutorial/login_phone_number.java
@@ -2,13 +2,45 @@ package com.example.tutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.hbb20.CountryCodePicker;
 
 public class login_phone_number extends AppCompatActivity {
+
+    CountryCodePicker countryCodePicker;
+    EditText phoneinput;
+    Button sendOtpbtn;
+    ProgressBar progressBar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_phone_number);
+
+        countryCodePicker = findViewById(R.id.login_countrycode);
+        phoneinput = findViewById(R.id.login_mobile_number);
+        sendOtpbtn =findViewById(R.id.send_otp_btn);
+        progressBar = findViewById(R.id.login_progress_bar);
+        progressBar.setVisibility(View.GONE);
+
+        countryCodePicker.registerCarrierNumberEditText(phoneinput);
+        sendOtpbtn.setOnClickListener((v)->{
+            if(!countryCodePicker.isValidFullNumber()){
+                phoneinput.setError("phone number not valid");
+                return;
+            }
+            Intent intent = new Intent(login_phone_number.this, LoginOtpActivity.class);
+
+            intent.putExtra("phone",countryCodePicker.getFullNumberWithPlus());
+            startActivity(intent);
+        });
     }
 }
