0c400803a8d0364426ab06a455804418ffd7b586
TommasoBiadene <tbiadene@gmail.com>
2023-11-30 10:37:05 +0100

    dio can

--- app/src/main/java/com/example/tutorial/LoginOtpActivity.java
+++ app/src/main/java/com/example/tutorial/LoginOtpActivity.java
@@ -1,20 +1,81 @@
 package com.example.tutorial;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class LoginOtpActivity extends AppCompatActivity {
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class
+LoginOtpActivity extends AppCompatActivity {
 
     String phoneNumber;
+    long timeoutSeconds = 60L;
+    EditText otpInput;
+    Button nextbtn;
+    ProgressBar progressBar;
+    TextView resendopttext;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    .l
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_otp);
 
+        otpInput = findViewById(R.id.login_otp);
+        nextbtn = findViewById(R.id.login_next_btn);
+        progressBar = findViewById(R.id.login_progress_bar);
+        resendopttext = findViewById(R.id.resend_otp_textview);
+
+
         phoneNumber = getIntent().getExtras().getString("phone");
-        Toast.makeText(getApplicationContext(), phoneNumber, Toast.LENGTH_SHORT).show();
+        sendOtp(phoneNumber,false);
+
+    }
+
+    void sendOtp(String phoneNumber,boolean isResend){
+        setInProgress(true);
+
+        PhoneAuthOptions.Builder builder = PhoneAuthOptions.newBuilder(mAuth)
+                .setPhoneNumber(phoneNumber)
+                .setTimeout(timeoutSeconds, TimeUnit.SECONDS)
+                .setActivity(this)
+                .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+                    }
+
+                    @Override
+                    public void onVerificationFailed(@NonNull FirebaseException e) {
+
+                    }
+                });
+    }
+
+    void setInProgress(boolean inProgress){
+        if (inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            nextbtn.setVisibility(View.GONE);
+        }else{
+            progressBar.setVisibility(View.GONE);
+        }
     }
 }
